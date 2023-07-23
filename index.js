b6bfe2ee4f9c04da8eb4c01339e898bf9c5d12a0
Michelle Kyalo <muendikyalo@gmail.com>
2023-07-23 11:13:51 +0300

    Done with functions lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    if (blocks < 0){
+        return "Distance can't be zero.";
+    }
+    else if (blocks <= 42){
+        return 42 - blocks;
+    }
+    else {
+        return blocks - 42;
+    }
+    }
+function distanceFromHqInFeet(blocks){
+    const distanceInFeet = 264;
+    return distanceFromHqInBlocks(blocks) * distanceInFeet;
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (start < destination){
+        return (destination - start) * 264;
+    }
+    else{
+        return (start - destination) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400){
+        return 0;
+    }
+    else if( distance > 400 && distance <= 2000){
+        return 0.02 * (distance - 400);
+    }
+    else if( distance > 2000 && distance < 2500){
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
